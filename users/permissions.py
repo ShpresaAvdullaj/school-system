3d6ceebd1db0c2093d7481983cb8029bf97081f4
Shpresa Avdullaj <shpresa.avdullaj@gmail.com>
2023-02-28 01:04:49 +0100

    fixing...

--- users/permissions.py
+++ users/permissions.py
@@ -1,28 +1,32 @@
 from rest_framework import permissions
 from rest_framework.exceptions import APIException
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class PermissionDenied(APIException):
     status_code = status.HTTP_403_FORBIDDEN
-    default_detail = 'You do not have permission to perform this action.'
+    default_detail = 'You do not have permission to perform this action or your access token has expired.'
     default_code = 'permission_denied'
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user.role == "ADMIN" and request.user.is_staff)
+        refresh = RefreshToken.for_user(request.user)
+        return bool(request.user.role == "ADMIN" and request.user.is_staff and refresh.blacklist() is False)
 
 
 class IsTeacherOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.role == "TEACHER":
+        refresh = RefreshToken.for_user(request.user)
+        if request.user.role == "TEACHER" and refresh.blacklist() is False:
             return True
         raise PermissionDenied()
 
 
 class IsStudentOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.role == "STUDENT":
+        refresh = RefreshToken.for_user(request.user)
+        if request.user.role == "STUDENT" and refresh.blacklist() is False:
             return True
         raise PermissionDenied()
